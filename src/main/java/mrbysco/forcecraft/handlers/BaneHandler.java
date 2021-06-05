0ff6f9c0f7f5b683d204d166f6f4c16acd7dfd4a
Mrbysco <Mrbysco@users.noreply.github.com>
2021-06-06 01:38:30 +0200

    Re-apply bane upon rejoining world

--- src/main/java/mrbysco/forcecraft/handlers/BaneHandler.java
+++ src/main/java/mrbysco/forcecraft/handlers/BaneHandler.java
@@ -1,6 +1,10 @@
 package mrbysco.forcecraft.handlers;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.goal.CreeperSwellGoal;
+import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -16,6 +20,24 @@ public class BaneHandler {
                     event.setCanceled(true);
                 }
             });
+        }
+    }
+
+    @SubscribeEvent
+    public void onEntityCreation(EntityJoinWorldEvent event) {
+        if(!event.getWorld().isRemote()) {
+            Entity entity = event.getEntity();
+            if(entity instanceof CreeperEntity){
+                CreeperEntity creeper = ((CreeperEntity) entity);
+                creeper.getCapability(CAPABILITY_BANE).ifPresent((entityCap) -> {
+                    if(entityCap.canExplode()){
+                        creeper.setCreeperState(-1);
+                        creeper.getDataManager().set(CreeperEntity.IGNITED, false);
+                        entityCap.setExplodeAbility(false);
+                        creeper.goalSelector.goals.removeIf(goal -> goal.getGoal() instanceof CreeperSwellGoal);
+                    }
+                });
+            }
         }
     }
 }
