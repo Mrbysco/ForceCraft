42a7e37471b0471f9d381da7661f73eef6eb8bb6
Mrbysco <Mrbysco@users.noreply.github.com>
2021-10-14 01:49:06 +0200

    Fix speed only being applied client-side [build]

--- src/main/java/com/mrbysco/forcecraft/handlers/PlayerCapHandler.java
+++ src/main/java/com/mrbysco/forcecraft/handlers/PlayerCapHandler.java
@@ -23,7 +23,7 @@ public class PlayerCapHandler {
 
 	@SubscribeEvent
 	public void onPlayerUpdate(TickEvent.PlayerTickEvent event) {
-		if(event.phase == TickEvent.Phase.END && event.player.world.isRemote) {
+		if(event.phase == TickEvent.Phase.END && !event.player.world.isRemote) {
 			PlayerEntity player = event.player;
 
 			Iterable<ItemStack> armor = player.getArmorInventoryList();
