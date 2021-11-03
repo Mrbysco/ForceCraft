52da24587fe39b1f3a30ec3b6e56e608013d8ee3
Mrbysco <Mrbysco@users.noreply.github.com>
2021-11-03 02:13:07 +0100

    Make Claws less rare and affected by looting [build]
    See #38

--- src/main/java/com/mrbysco/forcecraft/handlers/LootTableHandler.java
+++ src/main/java/com/mrbysco/forcecraft/handlers/LootTableHandler.java
@@ -5,6 +5,7 @@ import net.minecraft.loot.EmptyLootEntry;
 import net.minecraft.loot.ItemLootEntry;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.RandomValueRange;
+import net.minecraft.loot.functions.LootingEnchantBonus;
 import net.minecraft.loot.functions.SetCount;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.LootTableLoadEvent;
@@ -12,17 +13,19 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class LootTableHandler {
     public static final ResourceLocation BAT = new ResourceLocation("minecraft", "entities/bat");
+    // To test use: /summon bat ~ ~ ~ {NoAI:1b}
     @SubscribeEvent
     public void onLootTableLoadEvent(LootTableLoadEvent event) {
         if(event.getName().equals(BAT)) {
             LootPool.Builder builder = LootPool.builder();
             builder.addEntry(ItemLootEntry.builder(ForceRegistry.CLAW.get())
                     .weight(1)
-                    .acceptFunction(SetCount.builder(RandomValueRange.of(0, 2))))
+                    .acceptFunction(SetCount.builder(RandomValueRange.of(0.0F, 1.0F)))
+                    .acceptFunction(LootingEnchantBonus.builder(RandomValueRange.of(0.0F, 1.0F))))
                     .name("forcecraft_inject");
             builder.addEntry(EmptyLootEntry.func_216167_a()
-                    .weight(15)
-                    .acceptFunction(SetCount.builder(RandomValueRange.of(0, 2))))
+                    .weight(1)
+                    .acceptFunction(SetCount.builder(RandomValueRange.of(0.0F, 2.0F))))
                     .name("forcecraft_empty_roll");
             LootPool pool = builder.build();
 
