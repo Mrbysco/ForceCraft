d6cc10c4a2651db900a990fe8b3f9e1b45736c4f
Mrbysco <Mrbysco@users.noreply.github.com>
2022-02-18 01:55:22 +0100

    Fix the tree? [push]

--- src/main/java/com/mrbysco/forcecraft/world/WorldGenHandler.java
+++ src/main/java/com/mrbysco/forcecraft/world/WorldGenHandler.java
@@ -2,10 +2,11 @@ package com.mrbysco.forcecraft.world;
 
 import com.mrbysco.forcecraft.config.ConfigHandler;
 import com.mrbysco.forcecraft.world.feature.ForceFeatureConfigs;
-import com.mrbysco.forcecraft.world.feature.ForcePlacements;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biome.BiomeCategory;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -18,11 +19,12 @@ public class WorldGenHandler {
 		Biome.BiomeCategory category = event.getCategory();
 		if(category != BiomeCategory.THEEND && category != BiomeCategory.NETHER) {
 			if(ConfigHandler.COMMON.generateForceOre.get()) {
-				builder.getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ForcePlacements.ORE_FORCE);
-				builder.getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ForcePlacements.ORE_FORCE_BURIED);
+				builder.getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ForceFeatureConfigs.PLACED_ORE_FORCE);
+				builder.getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ForceFeatureConfigs.PLACED_ORE_FORCE_BURIED);
 			}
 			if(ConfigHandler.COMMON.generateForceTree.get() && category == BiomeCategory.FOREST) {
-				builder.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, ForcePlacements.FORCE_TREE_CHECKED);
+				builder.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,
+						ForceFeatureConfigs.TREES_FORCE.placed(VegetationPlacements.treePlacement(RarityFilter.onAverageOnceEvery(3))));
 			}
 		}
 	}
--- src/main/java/com/mrbysco/forcecraft/world/feature/ForceFeatureConfigs.java
+++ src/main/java/com/mrbysco/forcecraft/world/feature/ForceFeatureConfigs.java
@@ -3,18 +3,28 @@ package com.mrbysco.forcecraft.world.feature;
 import com.mrbysco.forcecraft.registry.ForceRegistry;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration.TargetBlockState;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.treedecorators.BeehiveDecorator;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
 import java.util.List;
 
@@ -27,16 +37,35 @@ public class ForceFeatureConfigs {
 			List.of(OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, FORCE_ORE)/*,
 					OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, Blocks.DEEPSLATE_DIAMOND_ORE.defaultBlockState())*/);
 
-	public static final ConfiguredFeature<?, ?> ORE_FORCE = FeatureUtils.register("ore_force", Feature.ORE.configured(new OreConfiguration(ORE_FORCE_TARGET_LIST, 6)));
-	public static final ConfiguredFeature<?, ?> ORE_FORCE_BURIED = FeatureUtils.register("ore_force_buried", Feature.ORE.configured(new OreConfiguration(ORE_FORCE_TARGET_LIST, 6, 1.0F)));
+	public static final ConfiguredFeature<?, ?> ORE_FORCE = FeatureUtils.register("forcecraft:ore_force", Feature.ORE.configured(new OreConfiguration(ORE_FORCE_TARGET_LIST, 6)));
+	public static final ConfiguredFeature<?, ?> ORE_FORCE_BURIED = FeatureUtils.register("forcecraft:ore_force_buried", Feature.ORE.configured(new OreConfiguration(ORE_FORCE_TARGET_LIST, 6, 1.0F)));
 
+	public static final PlacedFeature PLACED_ORE_FORCE = PlacementUtils.register("forcecraft:ore_force", OreFeatures.ORE_LAPIS.placed(commonOrePlacement(5, HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(48)))));
+	public static final PlacedFeature PLACED_ORE_FORCE_BURIED = PlacementUtils.register("forcecraft:ore_force_buried", OreFeatures.ORE_LAPIS_BURIED.placed(commonOrePlacement(3, HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(64)))));
 
-	public static final ConfiguredFeature<TreeConfiguration, ?> FORCE_TREE = FeatureUtils.register("force_tree",
+	private static List<PlacementModifier> orePlacement(PlacementModifier modifier1, PlacementModifier modifier2) {
+		return List.of(modifier1, InSquarePlacement.spread(), modifier2, BiomeFilter.biome());
+	}
+
+	private static List<PlacementModifier> commonOrePlacement(int count, PlacementModifier modifier) {
+		return orePlacement(CountPlacement.of(count), modifier);
+	}
+
+	public static final ConfiguredFeature<TreeConfiguration, ?> FORCE_TREE = FeatureUtils.register("forcecraft:force_tree",
 			ForceFeatures.FORCE_TREE.get().configured(createForceTree().build()));
 
 	private static final BeehiveDecorator BEEHIVE_005 = new BeehiveDecorator(0.05F);
-	public static final ConfiguredFeature<TreeConfiguration, ?> FORCE_TREE_WITH_MORE_BEEHIVES_CONFIG = FeatureUtils.register("force_tree_with_bees",
+	public static final ConfiguredFeature<TreeConfiguration, ?> FORCE_TREE_WITH_MORE_BEEHIVES_CONFIG = FeatureUtils.register("forcecraft:force_tree_with_bees",
 			ForceFeatures.FORCE_TREE.get().configured(createForceTree().decorators(List.of(BEEHIVE_005)).build()));
+
+	public static final PlacedFeature PLACED_FORCE_TREE = PlacementUtils.register("forcecraft:force_tree",
+			ForceFeatureConfigs.FORCE_TREE.filteredByBlockSurvival(ForceRegistry.FORCE_SAPLING.get()));
+	public static final PlacedFeature PLACED_FORCE_TREE_BEES_002 = PlacementUtils.register("forcecraft:force_tree_bees_002",
+			ForceFeatureConfigs.FORCE_TREE_WITH_MORE_BEEHIVES_CONFIG.filteredByBlockSurvival(ForceRegistry.FORCE_SAPLING.get()));
+
+	public static final ConfiguredFeature<RandomFeatureConfiguration, ?> TREES_FORCE = FeatureUtils.register("forcecraft:trees_force",
+			Feature.RANDOM_SELECTOR.configured(new RandomFeatureConfiguration(
+					List.of(new WeightedPlacedFeature(PLACED_FORCE_TREE_BEES_002, 0.2F)), PLACED_FORCE_TREE)));
 
 	private static TreeConfiguration.TreeConfigurationBuilder createStraightBlobTree(BlockState state, BlockState p_195148_, int p_195149_, int p_195150_, int p_195151_, int p_195152_) {
 		return new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(state), new StraightTrunkPlacer(p_195149_, p_195150_, p_195151_), BlockStateProvider.simple(p_195148_), new BlobFoliagePlacer(ConstantInt.of(p_195152_), ConstantInt.of(0), 3), new TwoLayersFeatureSize(1, 0, 1));
