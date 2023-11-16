24d4f9b82940bda929537ca6cc0eb994a578ec7d
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-16 15:50:30 +0100

    Datagen the ores tag

--- src/main/java/com/mrbysco/forcecraft/datagen/data/tags/ForceBlockTags.java
+++ src/main/java/com/mrbysco/forcecraft/datagen/data/tags/ForceBlockTags.java
@@ -11,13 +11,15 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nullable;
 
+import static com.mrbysco.forcecraft.registry.ForceRegistry.POWER_ORE;
+
 public class ForceBlockTags extends BlockTagsProvider {
 	public ForceBlockTags(DataGenerator generator, @Nullable ExistingFileHelper existingFileHelper) {
 		super(generator, Reference.MOD_ID, existingFileHelper);
 	}
 
-	public static final ITag.INamedTag<Block> RELOCATION_NOT_SUPPORTED = forgeTag("relocation_not_supported");
-	public static final ITag.INamedTag<Block> NON_MOVABLE = optionalTag("create", "non_movable");
+	public static final ITag.INamedTag<Block> ORES = forgeTag("ores");
+	public static final ITag.INamedTag<Block> ORES_POWER = forgeTag("ores/power");
 
 	private static ITag.INamedTag<Block> forgeTag(String name) {
 		return BlockTags.bind(new ResourceLocation("forge", name).toString());
@@ -29,6 +31,7 @@ public class ForceBlockTags extends BlockTagsProvider {
 
 	@Override
 	protected void addTags() {
-
+		this.tag(ORES_POWER).add(POWER_ORE.get());
+		this.tag(ORES).addTag(ORES_POWER);
 	}
 }
--- src/main/java/com/mrbysco/forcecraft/datagen/data/tags/ForceItemTags.java
+++ src/main/java/com/mrbysco/forcecraft/datagen/data/tags/ForceItemTags.java
@@ -1,18 +1,39 @@
 package com.mrbysco.forcecraft.datagen.data.tags;
 
 import com.mrbysco.forcecraft.Reference;
+import net.minecraft.block.Block;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
+import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
+
+import static com.mrbysco.forcecraft.registry.ForceRegistry.POWER_ORE;
+import static com.mrbysco.forcecraft.registry.ForceRegistry.POWER_ORE_ITEM;
 
 public class ForceItemTags extends ItemTagsProvider {
 	public ForceItemTags(DataGenerator dataGenerator, BlockTagsProvider blockTagsProvider, ExistingFileHelper existingFileHelper) {
 		super(dataGenerator, blockTagsProvider, Reference.MOD_ID, existingFileHelper);
 	}
 
+	public static final ITag.INamedTag<Item> ORES = forgeTag("ores");
+	public static final ITag.INamedTag<Item> ORES_POWER = forgeTag("ores/power");
+
+	private static ITag.INamedTag<Item> forgeTag(String name) {
+		return ItemTags.bind(new ResourceLocation("forge", name).toString());
+	}
+
+	private static ITag.INamedTag<Item> optionalTag(String modid, String name) {
+		return ItemTags.bind(new ResourceLocation(modid, name).toString());
+	}
+
 	@Override
 	protected void addTags() {
-
+		this.tag(ORES_POWER).add(POWER_ORE_ITEM.get());
+		this.tag(ORES).addTag(ORES_POWER);
 	}
 }
