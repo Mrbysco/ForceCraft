0b3131c9c2f54d7c1e47ff47ef5e9d2d99179ae9
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-28 00:56:10 +0200

    Update LumberHandler.java

--- src/main/java/com/mrbysco/forcecraft/handlers/LumberHandler.java
+++ src/main/java/com/mrbysco/forcecraft/handlers/LumberHandler.java
@@ -9,7 +9,7 @@ import net.neoforged.neoforge.event.level.BlockEvent;
 public class LumberHandler {
 
 	@SubscribeEvent
-	public void onTooltipEvent(BlockEvent.BreakEvent event) {
+	public void onBreakBlock(BlockEvent.BreakEvent event) {
 		final ItemStack stack = event.getPlayer().getMainHandItem();
 		if (stack.has(ForceComponents.TOOL_LUMBERJACK)) {
 			ForceAxeItem.fellTree(stack, event.getPos(), event.getPlayer());
